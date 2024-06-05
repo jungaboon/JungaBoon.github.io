d128c2d57cb98d3b1efd4f4af7c7149098847211
jungaboon <70823690+jungaboon@users.noreply.github.com>
2024-06-05 19:13:14 -0400

    Updated

--- script.js
+++ script.js
@@ -1,50 +1,42 @@
-const welcomeMsg = 
-[
-    "A collection of past projects.",
-    "Knowing the answer will always help you solve a problem.",
-    "They say you can't buy happiness, but you can always buy a dog.",
-    "There's more than one way to skin a cat.",
-    "Who wins in a fight? Captain America or Mike Tyson?",
-    "Who wins in a fight? Broly or Thor?",
-    "Who wins in a fight? Shazam or Captain Marvel?",
-    "Who wins in a fight? A potato or a broccoli?",
-    "These funny dinos.",
-    "Hey! What a wonderful kind of day!",
-    "Hello! Is it me you're looking for?",
-    "I ain't afraid of no ghost.",
-    "Who wins in a fight? The Ninja Turtles or the Power Rangers?",
-    "Which is heavier? A kilogram of steel or a kilogram of feathers?",
-    "Do you believe in life after love?",
-    "What is love? Baby don't hurt me!",
-    "Where we're going, we don't need roads.",
-    "You feeling lucky? Are you?",
-    "Under construction.",
-    "She's a maneater!",
-    "You wanna get nuts? Let's get nuts."
+const frontIcon = document.getElementById("front-icon");
+const jungaboonText = document.getElementById("jungaboon-text");
+const frontDivider = document.getElementById("front-divider");
+
+const jbTexts = [
+    "(gah-bah-goohl)",
+    "(jung-gah-boon)",
+    "(yaba-daba-dooh)",
+    "(bah-bah-booey)",
+    "(fin-fang-foom)",
+    "(bung-gah-joon)"
 ]
 
-var welcomeText = document.getElementById("welcomeMsg");
-var tabHoverSFX = document.getElementById("tabHover");
-var tab1 = document.getElementById("tab1");
-var tab2 = document.getElementById("tab2");
-var tab3 = document.getElementById("tab3");
+frontIcon.addEventListener("mousemove", ShiftIconToMousePos);
+frontIcon.addEventListener("mouseleave", ResetIconPos);
+SetJungaBoonText();
 
-tab1.addEventListener("mouseover", tab_hover);
-tab2.addEventListener("mouseover", tab_hover);
-tab3.addEventListener("mouseover", tab_hover);
-
-function load_welcome_msg()
+function ShiftIconToMousePos(event)
 {
-    let i = welcomeMsg.length;
-    let index = Math.floor(Math.random() * i);
+    const rect = frontIcon.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
 
-    welcomeText.innerHTML = welcomeMsg[index];
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const offsetX = (mouseX - centerX) * 0.1;
+    const offsetY = (mouseY - centerY) * 0.1;
+
+    frontIcon.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
 }
 
-function tab_hover()
+function ResetIconPos(event)
 {
-    tabHoverSFX.currentTime=0;
-    tabHoverSFX.play();
+    frontIcon.style.transform='translate(0,0)';
 }
 
-load_welcome_msg();
+function SetJungaBoonText()
+{
+    jungaboonText.innerHTML=jbTexts[Math.floor(Math.random()*jbTexts.length)];
+    frontDivider.style.width="60%";
+}
